8801ab666d05efd9874bace2ecb29df1035d0891
Manuela Viola <manuela.viola@rootstrap.com>
2022-09-22 17:44:27 -0300

    feat: yo nunca game

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo } from "react";
+
+import CardFlip from "./components/CardFlip/index.tsx";
+import { initialData } from "./data";
+import "./App.css";
 
 function App() {
+  const initialDataShuffled = useMemo(
+    () =>
+      initialData
+        .map((value) => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value),
+    [initialData]
+  );
+
+  const [data, setData] = useState(initialDataShuffled);
+
+  const handleOnClick = (e, { index, text, player, country, image }) => {
+    e.preventDefault();
+    setData((prevState) => {
+      const newData = [...prevState];
+      const selectedValue = prevState[index];
+      const newValue = {
+        ...selectedValue,
+        isFlipped: !selectedValue.isFlipped,
+      };
+      newData[index] = newValue;
+      return newData;
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>YO NUNCA</h1>
+        <div className="Container">
+          {data.map(({ text, player, country, image, isFlipped }, index) => (
+            <CardFlip isFlipped={isFlipped} key={index}>
+              <img
+                className="Card"
+                onClick={(e) =>
+                  handleOnClick(e, { index, text, player, country, image })
+                }
+                src={image}
+                alt={`${player}-${country}`}
+              ></img>
+
+              <p
+                className="Card Text"
+                onClick={(e) =>
+                  handleOnClick(e, { index, text, player, country, image })
+                }
+              >
+                {text}
+              </p>
+            </CardFlip>
+          ))}
+        </div>
       </header>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,158 @@
+import luisSuarez from "./assets/UyLuisSuarez.png";
+import neymar from "./assets/BrNeymar.png";
+import lionelMessi from "./assets/ArgLionelMessi.png";
+import porCristianoRonaldo from "./assets/PorCristianoRonaldo.png";
+import antoineGriezmann from "./assets/FrGriezmann.png";
+import raulJimenez from "./assets/MeRaulJimenez.png";
+import mattTurner from "./assets/USAMattTurner.png";
+import memphisDepay from "./assets/HolMemphisDepay.png";
+import robertLewandowski from "./assets/PolRobertLewandowski.png";
+import jackGrealish from "./assets/IngJackGrealish.png";
+import almoezAli from "./assets/CatAlmoezAli.png";
+import moisesCaicedo from "./assets/EcuMoisesCaicedo.png";
+import atibaHutchinson from "./assets/CanAtibaHutchinson.png";
+import mathewLeckie from "./assets/AusMathewLeckie.png";
+import heungMinSon from "./assets/CorHeungMinSon.png";
+import antonioRudiger from "./assets/AleAntonioRudiger.png";
+import sardarAzmoun from "./assets/IranSardarAzmoun.png";
+import thomasPartey from "./assets/GhaThomasPartey.png";
+import lukaModric from "./assets/CroLukaModric.png";
+import edenHazard from "./assets/BelEdenHazard.png";
+import chrisGunter from "./assets/GalChrisGunter.png";
+import yannSommer from "./assets/SuiYannSommer.png";
+
+export const initialData = [
+  {
+    text: "Me tropecé por caminar distraído mirando el celular",
+    player: "Raúl Jiménez",
+    country: "Mexico",
+    image: raulJimenez,
+  },
+  {
+    text: "Fui mordido por un animal",
+    player: "Luis Suarez",
+    country: "Uruguay",
+    image: luisSuarez,
+  },
+  {
+    text: "Me corté el pelo y luego me arrepentí",
+    player: "Lionel Messi",
+    country: "Argentina",
+    image: lionelMessi,
+  },
+  {
+    text: "Compré cosas en un gift shop sin entrar al museo",
+    player: "Matt Turner",
+    country: "Estados Unidos",
+    image: mattTurner,
+  },
+  {
+    text: "Completé un álbum de figuritas del mundial",
+    player: "Cristiano Ronaldo",
+    country: "Portugal",
+    image: porCristianoRonaldo,
+  },
+  {
+    text: "Me teñí el pelo con un color llamativo y/o fantasía",
+    player: "Antoine Griezmann",
+    country: "Francia",
+    image: antoineGriezmann,
+  },
+  {
+    text: "Me emborraché mucho hasta perder el conocimiento",
+    player: "Jack Grealish",
+    country: "Inglaterra",
+    image: jackGrealish,
+  },
+  {
+    text: "Me dormí en lugares inapropiados (oficina, misa, cine)",
+    player: "Atiba Hutchinson",
+    country: "Canada",
+    image: atibaHutchinson,
+  },
+  {
+    text: "Mandé un mensaje y/o screenshot a la persona equivocada",
+    player: "Robert Lewandowski",
+    country: "Polonia",
+    image: robertLewandowski,
+  },
+  {
+    text: "Compré pasajes sin las vacaciones aprobadas",
+    player: "Almoez Ali",
+    country: "Catar",
+    image: almoezAli,
+  },
+  {
+    text: "Fui a comprar algo y compré de todo menos eso",
+    player: "Moises Caicedo",
+    country: "Ecuador",
+    image: moisesCaicedo,
+  },
+  {
+    text: "Mentí jugando al Yo Nunca",
+    player: "Yann Sommer",
+    country: "Suiza",
+    image: yannSommer,
+  },
+  {
+    text: "Me hice un tatuaje",
+    player: "Memphis Depay",
+    country: "Holanda",
+    image: memphisDepay,
+  },
+  {
+    text: "Usé el cepillo de dientes de otra persona",
+    player: "Eden Hazard",
+    country: "Belgica",
+    image: edenHazard,
+  },
+  {
+    text: "Entré a una fiesta sin invitación",
+    player: "Luka Modric",
+    country: "Croacia",
+    image: lukaModric,
+  },
+  {
+    text: "Pagué por una clase de gimnasia y no fui",
+    player: "Sardar Azmoun",
+    country: "Iran",
+    image: sardarAzmoun,
+  },
+  {
+    text: "Volví a regalar un regalo que no quería",
+    player: "Thomas Partey",
+    country: "Ghana",
+    image: thomasPartey,
+  },
+  {
+    text: "Me cree una cuenta de Facebook o Instagram falsa",
+    player: "Chris Gunter",
+    country: "Gales",
+    image: chrisGunter,
+  },
+  {
+    text: "Grabé un video de mí mismo cantando o bailando",
+    player: "Neymar",
+    country: "Brasil",
+    image: neymar,
+  },
+  {
+    text: "Pasé horas viendo videos divertidos en YouTube/Tik Tok/Instagram",
+    player: "Mathew Leckie",
+    country: "Australia",
+    image: mathewLeckie,
+  },
+  {
+    text: "Saludé a alguien que pensaba que era otra persona",
+    player: "Heung Min Son",
+    country: "Corea",
+    image: heungMinSon,
+  },
+  {
+    text:
+      "Dije 'esto no funciona' y era error mio (no enchufado, faltaban pilas, etc)",
+    player: "Antonio Rudiger",
+    country: "Alemania",
+    image: antonioRudiger,
+  },
+];
